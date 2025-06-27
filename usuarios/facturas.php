aee105f13a9497a75c2ac98107adef595cb675a1
Jhon Oderman Mejia <jhooderman@gmail.com>
2025-06-27 13:21:24 -0500

    Forzar la relación con un cliente existente

--- usuarios/facturas.php
+++ usuarios/facturas.php
@@ -166,14 +166,14 @@ $paginaActual['pag_nombre'] = "Facturas";
 										<?php
 										if (isset($_GET["cte"]) and $_GET["cte"] != "") {
 											$consulta = mysqli_query($conexionBdPrincipal, "SELECT * FROM facturas
-											LEFT JOIN clientes ON cli_id=factura_cliente AND cli_id='" . $_GET["cte"] . "'
+											INNER JOIN clientes ON cli_id=factura_cliente AND cli_id='" . $_GET["cte"] . "'
 											WHERE factura_id_empresa='".$idEmpresa."'
 											ORDER BY factura_id DESC
 											LIMIT $inicio, $limite
 											");
 										} else {
 											$consulta = mysqli_query($conexionBdPrincipal, "SELECT * FROM facturas
-											LEFT JOIN clientes ON cli_id=factura_cliente
+											INNER JOIN clientes ON cli_id=factura_cliente
 											LEFT JOIN proveedores ON prov_id=factura_proveedor
 											INNER JOIN usuarios ON usr_id=factura_creador
 											WHERE factura_id=factura_id AND factura_id_empresa='".$idEmpresa."' $filtro
