9da43d2b2103f04d0f0ada515607fe2b9ab3f869
Jhon Oderman Mejia <jhooderman@gmail.com>
2025-07-20 20:30:46 -0500

    Corregir campo de usuario

--- usuarios/cotizaciones.php
+++ usuarios/cotizaciones.php
@@ -180,7 +180,7 @@ include("includes/head.php");
 										$no = 1;
 										while ($res = mysqli_fetch_array($consulta, MYSQLI_BOTH)) {
 
-											if ($datosUsuarioActual['uss_tipo'] != ADMIN) {
+											if ($datosUsuarioActual['usr_tipo'] != ADMIN) {
 												$consultaNumZ = $conexionBdPrincipal->query("SELECT * FROM zonas_usuarios 
 												WHERE zpu_usuario='" . $_SESSION["id"] . "' AND zpu_zona='" . $res['cli_zona'] . "'");
 												$numZ = $consultaNumZ->num_rows;
