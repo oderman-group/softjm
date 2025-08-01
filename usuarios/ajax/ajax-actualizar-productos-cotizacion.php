7300c2a4af07e7f88e39a78a1992041915b84d63
Jhon Oderman Mejia <jhooderman@gmail.com>
2025-08-01 16:51:19 -0500

    Se tienen en cuenta nuevas columnas al gestionar la cotización

--- usuarios/ajax/ajax-actualizar-productos-cotizacion.php
+++ usuarios/ajax/ajax-actualizar-productos-cotizacion.php
@@ -16,7 +16,7 @@ if (isset($_POST["producto"]) && count($_POST["producto"]) > 0) {
 
         //Se obtienen uno por uno los datos completos de los productos que vienen en el array y .
         $consulta=$conexionBdPrincipal->query("SELECT prod_id, prod_utilidad, prod_precio, prod_costo, prod_costo_dolar,
-        czpp_id, czpp_producto, czpp_valor, czpp_cotizacion 
+        czpp_id, czpp_producto, czpp_valor, czpp_cotizacion, prod_nombre, prod_descuento1, prod_existencias
                 FROM productos
                 LEFT JOIN cotizacion_productos ON czpp_producto=prod_id AND czpp_cotizacion='" . $_POST["id"] . "'
                 WHERE prod_id='" . $_POST["producto"][$contador] . "'");
@@ -36,24 +36,25 @@ if (isset($_POST["producto"]) && count($_POST["producto"]) > 0) {
                 $valorProducto = productosPrecioListaUSD($productoDatos['prod_utilidad'], $productoDatos['prod_costo_dolar']);
             }
 
-            $conexionBdPrincipal->query("INSERT INTO cotizacion_productos(czpp_cotizacion, czpp_producto, czpp_cantidad, czpp_impuesto, czpp_descuento, czpp_valor, czpp_orden, czpp_tipo, czpp_costo, czpp_utilidad_porcentaje)VALUES('" . $_POST["id"] . "','" . $_POST["producto"][$contador] . "', 1, 19, 0, '" . $valorProducto . "', '" . $numero . "', ".CZPP_TIPO_COTZ.", '".$productoDatos['prod_costo']."', '".$productoDatos['prod_utilidad']."')");
+            $conexionBdPrincipal->query("INSERT INTO cotizacion_productos(czpp_cotizacion, czpp_producto, czpp_cantidad, czpp_impuesto, czpp_descuento, czpp_valor, czpp_orden, czpp_tipo, czpp_costo, czpp_utilidad_porcentaje, czpp_nombre_original, czpp_descuento_maximo_original, czpp_productos_existencias, czpp_precio_original)VALUES('" . $_POST["id"] . "','" . $_POST["producto"][$contador] . "', 1, 19, 0, '" . $valorProducto . "', '" . $numero . "', ".CZPP_TIPO_COTZ.", '".$productoDatos['prod_costo']."', '".$productoDatos['prod_utilidad']."', '".$productoDatos['prod_nombre']."', '".$productoDatos['prod_descuento1']."', '".$productoDatos['prod_existencias']."', '" . $valorProducto . "')");
 
-            } else {
-                if ($_POST["monedaActual"] != $_POST["moneda"]) {
-                    
-                    //Si cambió a pesos colombianos
-                    if ($_POST["moneda"] == 1) {
-                        $valorProducto = round(($productoDatos['czpp_valor'] * $configuracion['conf_trm_venta']), 0);
-                    }
-                    //Si cambió a Dolares
-                    else {
-                        
-                        $valorProducto = productosPrecioListaUSD($productoDatos['prod_utilidad'], $productoDatos['prod_costo_dolar']);
-                    }
-
-                    $conexionBdPrincipal->query("UPDATE cotizacion_productos SET czpp_valor='" . $valorProducto . "' WHERE czpp_id='" . $productoDatos['czpp_id'] . "'");
+        } else {
+            if ($_POST["monedaActual"] != $_POST["moneda"]) {
+                
+                //Si cambió a pesos colombianos
+                if ($_POST["moneda"] == 1) {
+                    $valorProducto = round(($productoDatos['czpp_valor'] * $configuracion['conf_trm_venta']), 0);
                 }
+                //Si cambió a Dolares
+                else {
+                    
+                    $valorProducto = productosPrecioListaUSD($productoDatos['prod_utilidad'], $productoDatos['prod_costo_dolar']);
+                }
+
+                $conexionBdPrincipal->query("UPDATE cotizacion_productos SET czpp_valor='" . $valorProducto . "', czpp_ultima_actualizacion=now(), czpp_cantidad_actualizaciones=czpp_cantidad_actualizaciones+1, czpp_usuario_ultima_actualizacion='".$_SESSION["id"]."'
+                WHERE czpp_id='" . $productoDatos['czpp_id'] . "'");
             }
+        }
 
             $contador++;
     endwhile;
@@ -80,4 +81,15 @@ if (isset($_POST["producto"]) && count($_POST["producto"]) > 0) {
     // Elimina todos los productos de la cotización cuando no se proporcionan productos
     $conexionBdPrincipal->query("DELETE FROM cotizacion_productos WHERE czpp_cotizacion='" . $_POST["id"] . "' AND czpp_servicio IS NULL AND czpp_combo IS NULL");
 }
-?>
+
+$infoActualizar = [
+    'tabla'          => 'cotizacion',
+    'clave_primaria' => 'cotiz_id',
+    'id_registro'    => $_POST["id"],
+    'sucp_id_empresa'=> $idEmpresa
+];
+$camposActualizar = [
+    'cotiz_version' => 'INCREMENT_BY_ONE'
+];
+
+Cotizacion::actualizarRegistro($infoActualizar, $camposActualizar);
