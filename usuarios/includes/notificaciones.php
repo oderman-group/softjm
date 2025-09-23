3045a9440b57e3d69328980d10e173b8ce7eeaee
Jhon Oderman Mejia <jhooderman@gmail.com>
2025-09-23 14:50:16 -0500

    Crear nuevo mensaje de notificación

--- usuarios/includes/notificaciones.php
+++ usuarios/includes/notificaciones.php
@@ -114,6 +114,13 @@ if(isset($_GET["msg"])){
 		</div>
 	<?php }?>
 
+	<?php if($_GET["msg"]==17){?>
+		<div class="alert alert-info">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+			<i class="icon-exclamation-sign"></i><strong>Información!</strong> <?=$_GET["msgContent"]?>
+		</div>
+	<?php }?>
+
 
 <?php }?>
 
