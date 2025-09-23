d5977b1fcde5db2d98a5c66bd25aa475d990d95d
Jhon Oderman Mejia <jhooderman@gmail.com>
2025-09-23 14:50:04 -0500

    Ajuste en la creación y edición de cotizaciones

--- usuarios/bd_create/cotizaciones-guardar.php
+++ usuarios/bd_create/cotizaciones-guardar.php
@@ -2,23 +2,39 @@
 require_once("../sesion.php");
 
 $idPagina = 29;
-if(isset($_POST["envio"])){
-    $envio=$_POST["envio"];
-}else{
-    $envio=0;
+
+if (isset($_POST["envio"])) {
+    $envio = $_POST["envio"];
+} else {
+    $envio = 0;
 }
 
-$consulta=$conexionBdPrincipal->query("SELECT * FROM clientes WHERE cli_id='".$_POST["cliente"]."'");
-$datosCliente = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+$ticketId = "NULL";
 
-    $conexionBdPrincipal->query("INSERT INTO cotizacion(cotiz_fecha_propuesta, cotiz_cliente, cotiz_fecha_vencimiento, cotiz_vendedor, cotiz_creador, cotiz_sucursal, cotiz_contacto, cotiz_forma_pago, cotiz_fecha_creacion, cotiz_moneda, cotiz_observaciones, cotiz_envio, cotiz_proveedor, cotiz_id_empresa)VALUES('" . $_POST["fechaPropuesta"] . "','" . $_POST["cliente"] . "','" . $_POST["fechaVencimiento"] . "','" . $_POST["influyente"] . "','" . $_SESSION["id"] . "','" . $_POST["sucursal"] . "','" . $_POST["contacto"] . "','" . $_POST["formaPago"] . "',now(),'" . $_POST["moneda"] . "','" . $_POST["notas"] . "','" . $envio . "','" . $_POST["proveedor"] . "','" . $idEmpresa . "' )");
-    $idInsert = mysqli_insert_id($conexionBdPrincipal);
+if ($_POST["ticket"] == 'TICKET_AUTO') {
+    //Creamos un ticket automáticamente
+    mysqli_query($conexionBdPrincipal,"INSERT INTO clientes_tikets(tik_asunto_principal, tik_tipo_tiket, tik_fecha_creacion, tik_usuario_responsable, tik_estado, tik_cliente, tik_prioridad, tik_sucursal, tik_etapa, tik_tipo_negocio)VALUES('COTIZACIÓN (Ticket creado de forma automática)', 1,'" . $_POST["fechaPropuesta"] . "','" . $_SESSION["id"] . "', 1,'" . $_POST["cliente"] . "', 2,'" . $_POST["sucursal"] . "', 3, 1)");
 
-    require("guardar-productos-cotizacion.php");
+    $ticketId = mysqli_insert_id($conexionBdPrincipal);
+} else if ($_POST["ticket"] != 'NO_TICKET') {
+    $ticketId = $_POST["ticket"];
+}
 
-    require("guardar-combos-cotizacion.php");
+$conexionBdPrincipal->query("INSERT INTO cotizacion(cotiz_fecha_propuesta, cotiz_cliente, cotiz_fecha_vencimiento, cotiz_vendedor, cotiz_creador, cotiz_sucursal, cotiz_contacto, cotiz_forma_pago, cotiz_fecha_creacion, cotiz_moneda, cotiz_observaciones, cotiz_envio, cotiz_proveedor, cotiz_id_empresa, cotiz_ticket)VALUES('" . $_POST["fechaPropuesta"] . "','" . $_POST["cliente"] . "','" . $_POST["fechaVencimiento"] . "','" . $_POST["influyente"] . "','" . $_SESSION["id"] . "','" . $_POST["sucursal"] . "','" . $_POST["contacto"] . "','" . $_POST["formaPago"] . "',now(),'" . $_POST["moneda"] . "','" . $_POST["notas"] . "','" . $envio . "','" . $_POST["proveedor"] . "','" . $idEmpresa . "', ".$ticketId.")");
+$idInsert = mysqli_insert_id($conexionBdPrincipal);
 
-    include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
+if ($_POST["ticket"] != 'NO_TICKET') {
+    mysqli_query($conexionBdPrincipal,"UPDATE clientes_tikets 
+    SET tik_id_cotizacion = ".$idInsert.",
+    tik_asunto_principal = 'COTIZACIÓN Nro. ".$idInsert." (Ticket creado de forma automática)' 
+    WHERE tik_id=".$ticketId);
+}
 
-    echo '<script type="text/javascript">window.location.href="../cotizaciones-editar.php?id=' . $idInsert . '&msg=1";</script>';
-    exit();
+require("guardar-productos-cotizacion.php");
+
+require("guardar-combos-cotizacion.php");
+
+include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
+
+echo '<script type="text/javascript">window.location.href="../cotizaciones-editar.php?id=' . $idInsert . '&msg=1";</script>';
+exit();
--- usuarios/bd_update/cotizaciones-actualizar.php
+++ usuarios/bd_update/cotizaciones-actualizar.php
@@ -14,30 +14,72 @@ include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
 $consultaCli=$conexionBdPrincipal->query("SELECT * FROM clientes WHERE cli_id='".$_POST["cliente"]."'");
 $datosCliente = mysqli_fetch_array($consultaCli, MYSQLI_BOTH);
 
-$conexionBdPrincipal->query("UPDATE cotizacion SET 
-cotiz_fecha_propuesta='" . $_POST["fechaPropuesta"] . "', 
-cotiz_cliente='" . $_POST["cliente"] . "', 
-cotiz_sucursal='" . $_POST["sucursal"] . "', 
-cotiz_contacto='" . $_POST["contacto"] . "', 
-cotiz_fecha_vencimiento='" . $_POST["fechaVencimiento"] . "', 
-cotiz_vendedor='" . $_POST["influyente"] . "', 
-cotiz_forma_pago='" . $_POST["formaPago"] . "', 
-cotiz_moneda='" . $_POST["moneda"] . "', 
-cotiz_ultima_modificacion=now(), 
-cotiz_usuario_modificacion='" . $_SESSION["id"] . "', 
-cotiz_observaciones='" . $conexionBdPrincipal->real_escape_string($_POST["notas"]) . "', 
-cotiz_envio='" . $_POST["envio"] . "', 
-cotiz_ocultar_descuento_combo='" . $_POST["dctoCombos"] . "', 
-cotiz_descuentos_especiales='" . $_POST["dctoEspecial"] . "', 
-cotiz_version=cotiz_version+1
-WHERE cotiz_id='" . $_POST["id"] . "' AND  cotiz_id_empresa='".$_SESSION["dataAdicional"]["id_empresa"]."'");
+try {
+    mysqli_query($conexionBdPrincipal, "START TRANSACTION");
 
-if($_POST["monedaActual"] != $_POST["moneda"]){
-    require('actualizar-productos-cotizacion-2.php');
-    require('actualizar-combos-cotizacion-2.php');
+    if (isset($_POST["ticket"])) {
+        $ticketId = "NULL";
+
+        if ($_POST["ticket"] == 'TICKET_AUTO') {
+            //Creamos un ticket automáticamente
+            mysqli_query($conexionBdPrincipal,"INSERT INTO clientes_tikets(tik_asunto_principal, tik_tipo_tiket, tik_fecha_creacion, tik_usuario_responsable, tik_estado, tik_cliente, tik_prioridad, tik_sucursal, tik_etapa, tik_tipo_negocio, tik_id_cotizacion)VALUES('COTIZACIÓN Nro. ".$_POST["id"]." (Ticket creado de forma automática)', 1,'" . $_POST["fechaPropuesta"] . "','" . $_SESSION["id"] . "', 1,'" . $_POST["cliente"] . "', 2,'" . $_POST["sucursal"] . "', 3, 1, ".$_POST["id"].")");
+
+            $ticketId = mysqli_insert_id($conexionBdPrincipal);
+        } else if ($_POST["ticket"] != 'NO_TICKET') {
+            $ticketId = $_POST["ticket"];
+            mysqli_query($conexionBdPrincipal,"UPDATE clientes_tikets 
+            SET tik_id_cotizacion = ".$idInsert.",
+            WHERE tik_id=".$ticketId);
+        }
+    }
+
+    $conexionBdPrincipal->query("UPDATE cotizacion SET 
+    cotiz_fecha_propuesta='" . $_POST["fechaPropuesta"] . "', 
+    cotiz_cliente='" . $_POST["cliente"] . "', 
+    cotiz_sucursal='" . $_POST["sucursal"] . "', 
+    cotiz_contacto='" . $_POST["contacto"] . "', 
+    cotiz_fecha_vencimiento='" . $_POST["fechaVencimiento"] . "', 
+    cotiz_vendedor='" . $_POST["influyente"] . "', 
+    cotiz_forma_pago='" . $_POST["formaPago"] . "', 
+    cotiz_moneda='" . $_POST["moneda"] . "', 
+    cotiz_usuario_modificacion='" . $_SESSION["id"] . "', 
+    cotiz_observaciones='" . $conexionBdPrincipal->real_escape_string($_POST["notas"]) . "', 
+    cotiz_envio='" . $_POST["envio"] . "', 
+    cotiz_ocultar_descuento_combo='" . $_POST["dctoCombos"] . "', 
+    cotiz_descuentos_especiales='" . $_POST["dctoEspecial"] . "', 
+    cotiz_ticket=" . $ticketId . "
+    WHERE cotiz_id='" . $_POST["id"] . "' 
+    AND cotiz_id_empresa='".$_SESSION["dataAdicional"]["id_empresa"]."'
+    ");
+
+    $filasAfectadas = $conexionBdPrincipal->affected_rows;
+
+    if ($filasAfectadas > 0) {
+        $conexionBdPrincipal->query("UPDATE cotizacion SET 
+        cotiz_ultima_modificacion=now(), 
+        cotiz_version=cotiz_version+1
+        WHERE cotiz_id='" . $_POST["id"] . "' 
+        AND cotiz_id_empresa='".$_SESSION["dataAdicional"]["id_empresa"]."'
+        ");
+
+        $parmetros = '&msg=2';
+    } else {
+        $parmetros = '&msg=17&msgContent=No hubo cambios para guardar.';
+    }
+
+    if ($_POST["monedaActual"] != $_POST["moneda"]) {
+        require('actualizar-productos-cotizacion-2.php');
+        require('actualizar-combos-cotizacion-2.php');
+    }
+
+    mysqli_query($conexionBdPrincipal, "COMMIT");
+
+} catch (Exception $e) {
+    mysqli_query($conexionBdPrincipal, "ROLLBACK");
+    $parmetros = '&error=3&message=Hubo problemas para actualizar esta cotización!';
 }
 
 include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
 
-echo '<script type="text/javascript">window.location.href="../cotizaciones-editar.php?id=' . $_POST["id"] . '&msg=2";</script>';
+echo '<script type="text/javascript">window.location.href="../cotizaciones-editar.php?id=' . $_POST["id"] . $parmetros.'";</script>';
 exit();
