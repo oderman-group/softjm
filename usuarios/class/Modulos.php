a56e524f6ef60e36a5f2eab6ce1f3bdab04c162b
Jhon Oderman Mejia <jhooderman@gmail.com>
2025-10-08 06:07:17 -0500

    ajuste de code style

--- usuarios/class/Modulos.php
+++ usuarios/class/Modulos.php
@@ -13,7 +13,7 @@ class Modulos {
     **/
     public static function validarAccesoModulo($empresa, $modulo, $conexionBdAdmin, $datosUsuarioActual){
 
-        if($datosUsuarioActual['usr_tipo']==1){
+        if ($datosUsuarioActual['usr_tipo'] == ADMIN) {
             return true;
         }
 
@@ -21,7 +21,7 @@ class Modulos {
         WHERE mxe_id_empresa='".$empresa."' AND mxe_id_modulo='".$modulo."'");
         $numRegistros = $consulta->num_rows;
         
-        if($numRegistros>0){
+        if ($numRegistros>0) {
             return true;
         }
 
@@ -47,7 +47,7 @@ class Modulos {
         $configuracion
     ) {
 
-        if ($datosUsuarioActual['usr_tipo'] == 1) { 
+        if ($datosUsuarioActual['usr_tipo'] == ADMIN) { 
             return true;
         }
 
@@ -55,19 +55,21 @@ class Modulos {
         WHERE upr_id_usuario='".$datosUsuarioActual['usr_id']."' AND upr_id_empresa='".$configuracion['conf_id_empresa']."'");
         
         $numRoles=mysqli_num_rows($consultaRoles);
-        if ($numRoles<1) { 
+
+        if ($numRoles < 1) { 
             return true;
-        }else{
+        } else {
             $Roles = mysqli_fetch_all($consultaRoles, MYSQLI_ASSOC);
             $valoresArray = array_column($Roles, 'upr_id_rol');
             $valoresCadena = implode(',', $valoresArray);
             
             $consultaPaginaRoles = mysqli_query($conexionBdPrincipal, "SELECT * FROM paginas_perfiles 
             WHERE pper_tipo_usuario IN ($valoresCadena)");
-                
+
             $RolesPaginas = mysqli_fetch_all($consultaPaginaRoles, MYSQLI_ASSOC);
             $valoresPaginas = array_column($RolesPaginas, 'pper_pagina');
             $permitidos= array_intersect($paginas,$valoresPaginas);
+
             if(!empty($permitidos)){
                 return true;
             }
