50b24011f64503b2428ab3016fd7efd82e6ea3bf
Jhon Oderman Mejia <jhooderman@gmail.com>
2025-10-22 07:49:55 -0500

    fix: Corregir enlaces CSS problemáticos que causaban errores 404
    - Comentar enlaces a Bootstrap y Toastr que causaban errores 404
    - Resolver problemas de carga de estilos externos
    - Mejorar estabilidad de la página de roles

--- usuarios/includes/head.php
+++ usuarios/includes/head.php
@@ -28,7 +28,7 @@
 <link href="css/bootstrap-responsive.css" rel="stylesheet">
 <link rel="stylesheet" href="css/font-awesome.css">
 <!--drag and drop del menu-->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.2/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.2/css/bootstrap.min.css"> -->
 <link rel="stylesheet" href="css/tooltip.css">
 <script src="https://kit.fontawesome.com/e84fa1cf78.js" crossorigin="anonymous"></script>
 
@@ -44,7 +44,7 @@
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-<link href="node_modules/toastr/build/toastr.min.css" rel="stylesheet">
+<!-- <link href="node_modules/toastr/build/toastr.min.css" rel="stylesheet"> -->
 
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-T5Q7PCSPK9"></script>
